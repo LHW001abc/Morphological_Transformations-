0076fff6906b1522ce8f43f7de92450d442a82b5
LHW001abc <abdelmajidzaddi088@gmail.com>
2024-09-24 23:49:57 +0200

    this is about image proc

--- Smothing_images.py
+++ Smothing_images.py
@@ -5,6 +5,14 @@ img = cv.imread('early_1800.jpg', 0)
 
 #average = cv.blur(img, (5, 5))
 gaussian = cv.GaussianBlur(img, (5, 5), 0)
+
+median = cv.medianBlur(img, 5)
+bilateral = cv.bilateralFilter(img, 9, 75, 75)
+cv.imshow('Bilateral', bilateral)
+
+
+cv.imshow('Image', img)
+cv.imshow('Median', median)
 #cv.imshow('Average', average)
 cv.imshow('Gaussian', gaussian)
 cv.waitKey(0)
