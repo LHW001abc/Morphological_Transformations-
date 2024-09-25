4f8bcdc714ce1f2b013594287e71c5a06700a85e
LHW001abc <abdelmajidzaddi088@gmail.com>
2024-09-25 11:01:40 +0200

    this is about image proc

--- Image_gradioent.py
+++ Image_gradioent.py
@@ -9,5 +9,24 @@ cv.imshow('Image', img)
 cv.imshow('Laplacian', lap)
 cv.waitKey(0)
 cv.destroyAllWindows()
- 
+
+#using sobelx sobely
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('messi5.jpg', cv.IMREAD_GRAYSCALE)
+sobelx = cv.Sobel(img, cv.CV_64F, 1, 0)
+sobely = cv.Sobel(img, cv.CV_64F, 0, 1)
+
+sobelx = np.uint8(np.absolute(sobelx))
+sobely = np.uint8(np.absolute(sobely))
+cv.imshow('Image', img)
+cv.imshow('SobelX', sobelx)
+cv.imshow('SobelY', sobely)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+
+
+
 
